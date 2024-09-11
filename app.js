13bc631f40a8d8380f3602f560dd8fec288fcfca
guadalupegarciac842 <garciacastellanoguadalupe@gmail.com>
2024-09-11 15:52:18 -0300

    tp 11 estructura mercadoLiebre

--- app.js
+++ app.js
@@ -1,23 +1,15 @@
-//módulos
 
-const express = require("express");
-const path = require("path");
-
-//puerto del servidor
+const express = require('express');
+const path = require('path');
 const PORT = 3000;
-
-//ejecución express
 const app = express();
 
-//recursos estáticos
 app.use(express.static(path.join(__dirname, "public")));
 
-//rutas
 app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "views", "home.html"))
 );
 
-//levantar el servidor
 app.listen(PORT, () =>
   console.log("Server running in http://localhost:" + PORT)
 );
